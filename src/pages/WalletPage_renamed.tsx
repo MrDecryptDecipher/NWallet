fbac92929c76fcbce39f05363402e45b67d8ff80
Sandeep Kumar Sahoo <sandeep.savethem2@gmail.com>
2025-12-30 03:38:02 +0530

    Repository Cleanup: Removed logs and temporary build artifacts

--- src/pages/WalletPage_renamed.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-// Legacy component - placeholder to passing build
-export const WalletPage: React.FC = () => {
-  return <div>Wallet Page</div>;
-};
-export default WalletPage;
